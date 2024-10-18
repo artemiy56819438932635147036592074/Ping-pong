d212aea019bb48dfca499f1678dc24be5bf478d9
artemiy56819438932635147036592074 <87006347+artemiy56819438932635147036592074@users.noreply.github.com>
2024-10-18 21:02:53 +0300

    V1.1
    Добавлен мяч
    restart не работает :(

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ win_height = 500
 window = display.set_mode((win_width,win_height))
 display.set_caption("ping pong")
 
+clock = time.Clock()
+
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x,player_y,player_speed,size_x,size_y):
         super().__init__()
@@ -35,8 +37,24 @@ class Player(GameSprite):
 
 background = transform.scale(image.load("background.jpg"), (win_width,win_height))
 
-racket_l = Player("racket.png",25,win_height/2,5,39,136)
+racket_l = Player("racket.png",25,win_height/2,10,39,136)
 racked_r = Player("racket.png",win_width-50,win_height/2,5,39,136)
+ball = GameSprite("tenis_ball.png",win_width/2,win_height/2,10,50,50)
+
+font.init()
+
+font2 = font.SysFont("Arial",72)
+Pl1_win = font2.render("PLAYER 2 LOSE",1,(255,0,0))
+Pl2_win = font2.render("PLAYER 1 LOSE",1,(255,0,0))
+reatart_label = font2.render("r-restart",1,(255,0,0))
+
+ball_speedx = 5
+ball_speedy = 5
+
+def restart():
+    finish = False
+    ball.rect.x = win_width/2
+    ball.rect.y = win_height/2
 
 game = True
 finish = False
@@ -44,11 +62,34 @@ while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+        elif e.type == KEYDOWN:
+            if e.key == K_r:
+                restart()
+
     
     if finish != True:
+        ball.rect.x += ball_speedx
+        ball.rect.y += ball_speedy
         window.blit(background,(0,0))
+        ball.reset()
         racket_l.reset()
         racket_l.update_l()
         racked_r.reset()
         racked_r.update_r()
+        if ball.rect.y <= 0:
+            ball_speedy *= -1
+        if ball.rect.y >= win_height-50:
+            ball_speedy *= -1
+        if sprite.collide_rect(ball,racket_l) or sprite.collide_rect(ball,racked_r):
+            ball_speedx *= -1
+        if ball.rect.x < 0:
+            window.blit(Pl2_win,(100,win_height/2))
+            window.blit(reatart_label,(150,(win_height/2)+ 50))
+            finish = True
+        if ball.rect.x > win_width:
+            window.blit(Pl1_win,(100,win_height/2))
+            window.blit(reatart_label,(150,(win_height/2)+50))
+            finish = True
+
     display.update()
+    clock.tick(40)
